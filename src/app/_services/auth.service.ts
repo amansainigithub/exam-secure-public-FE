27895585c468390fafd42a4067e8f310e5a63e5f
aman <amansainigithub>
2022-06-19 00:53:14 +0530

    some chnages in angular.json and minor URL changes

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:5000/api/public/auth/cred/';
-// const AUTH_API = 'http://examsecure-env.eba-3xmk9bvg.us-east-1.elasticbeanstalk.com/api/public/auth/cred/';
+// const AUTH_API = 'http://localhost:5000/api/public/auth/cred/';
+const AUTH_API = 'http://examsecure-env.eba-3xmk9bvg.us-east-1.elasticbeanstalk.com/api/public/auth/cred/';
 
 
 const httpOptions = {
