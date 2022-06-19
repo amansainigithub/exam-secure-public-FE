185b6079eb634397e2a96251de77828af53fa437
aman <amansainigithub>
2022-06-19 23:10:01 +0530

    MIROR CH...

--- src/app/_helpers/url-helper.service.ts
+++ src/app/_helpers/url-helper.service.ts
@@ -9,9 +9,9 @@ import { Injectable } from '@angular/core';
 })
 export class UrlHelperService {
 
-//   API_URL:any = 'http://localhost:5000/api/public/auth/cred/';
+  API_URL:any = 'http://localhost:5000/api/public/auth/cred/';
 
-  API_URL:any = 'http://examsecure-env.eba-3xmk9bvg.us-east-1.elasticbeanstalk.com/api/public/auth/cred/';
+  // API_URL:any = 'http://examsecure-env.eba-3xmk9bvg.us-east-1.elasticbeanstalk.com/api/public/auth/cred/';
 
 
   constructor() { }
