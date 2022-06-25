77da8ae325fe7534ead2a94be9a7957ed1c6e61f
aman <amansainigithub>
2022-06-26 01:10:43 +0530

    change ursl

--- src/app/_helpers/url-helper.service.ts
+++ src/app/_helpers/url-helper.service.ts
@@ -9,9 +9,9 @@ import { Injectable } from '@angular/core';
 })
 export class UrlHelperService {
 
-  // API_URL:any = 'http://localhost:5000/api/public/auth/cred/';
+  API_URL:any = 'http://localhost:5000/api/public/auth/cred/';
 
-   API_URL:any = 'https://146.190.233.4:5000/api/public/auth/cred/';
+  //  API_URL:any = 'https://146.190.233.4:5000/api/public/auth/cred/';
 
     //  API_URL:any = 'http://examsecure-env.eba-3xmk9bvg.us-east-1.elasticbeanstalk.com/api/public/auth/cred/';
 
