f09c62fdfd394fe21b86b525589bdcaf788f05b8
aman <amansainigithub>
2022-06-26 12:58:05 +0530

    url change

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:5000/api/public/auth/cred/';
-// const AUTH_API = 'https://146.190.233.4:5000/api/public/auth/cred/';
+// const AUTH_API = 'http://localhost:5000/api/public/auth/cred/';
+const AUTH_API = 'http://206.189.96.51:5000/api/public/auth/cred/';
 // const AUTH_API = 'http://examsecure-env.eba-3xmk9bvg.us-east-1.elasticbeanstalk.com/api/public/auth/cred/';
 
 
