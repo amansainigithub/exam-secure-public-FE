72a7f7318bfcc513f2e24869805295aab4c6a46f
aman <amansainigithub>
2022-07-04 03:04:47 +0530

    new

--- src/app/_services/categoryServices/branch-service/branch.service.ts
+++ src/app/_services/categoryServices/branch-service/branch.service.ts
@@ -14,19 +14,19 @@ export class BranchService {
   getBranchList()
   {
 
-    console.log("im here.................");
-    this.http.get("https://heroku-app-spring-boot.herokuapp.com/getList").subscribe(data=>{
-      console.log(data);
+    // console.log("im here.................");
+    // this.http.get("https://heroku-app-spring-boot.herokuapp.com/getList").subscribe(data=>{
+    //   console.log(data);
       
-    },error=>{
-      console.log(error);
-    })
-    this.http.get("https://heroku-app-spring-boot.herokuapp.com/saveobj").subscribe(data=>{
-     console.log("+++++++++++++++++++++++++++++++++++++++++++");
-     console.log(data);
-    },error=>{
-      console.log(error);
-    })
+    // },error=>{
+    //   console.log(error);
+    // })
+    // this.http.get("https://heroku-app-spring-boot.herokuapp.com/saveobj").subscribe(data=>{
+    //  console.log("+++++++++++++++++++++++++++++++++++++++++++");
+    //  console.log(data);
+    // },error=>{
+    //   console.log(error);
+    // })
 
     return  this.http.get(this._AUTH_URL.API_URL + 'getBranchCategoryPublic');
   }
