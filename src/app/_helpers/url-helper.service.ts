57ff17d1b63ee83253528b25c099cf60cdd672df
aman <amansainigithub>
2022-07-04 19:20:24 +0530

    URL chnage

--- src/app/_helpers/url-helper.service.ts
+++ src/app/_helpers/url-helper.service.ts
@@ -11,8 +11,8 @@ export class UrlHelperService {
 
   // API_URL:any = 'http://localhost:5000/api/public/auth/cred/';
 
-   API_URL:any = 'http://103.93.16.121:33146/grabquiz/api/public/auth/cred/';
-  //  API_URL:any = 'http://grabquiz.tech/api/public/auth/cred/';
+  //  API_URL:any = 'http://103.93.16.121:33146/grabquiz/api/public/auth/cred/';
+   API_URL:any = 'https://grabquiz.tech/api/public/auth/cred/';
 
 
   constructor() { }
--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -3,8 +3,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 // const AUTH_API = 'http://localhost:5000/api/public/auth/cred/';
-const AUTH_API = 'http://103.93.16.121:33146/grabquiz/api/public/auth/cred/';
-// const AUTH_API = 'http://grabquiz.tech/api/public/auth/cred/';
+// const AUTH_API = 'http://103.93.16.121:33146/grabquiz/api/public/auth/cred/';
+const AUTH_API = 'https://grabquiz.tech/api/public/auth/cred/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
